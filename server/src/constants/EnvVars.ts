d401cefb67b903c50504b0c65fea81d0c5270695
Artem <updatethatpl0x@gmail.com>
2023-02-14 13:47:11 -0500

    removed unnecessary files

--- server/src/constants/EnvVars.ts
+++ server/src/constants/EnvVars.ts
@@ -4,25 +4,32 @@
 
 /* eslint-disable node/no-process-env */
 
-
 export default {
-  NodeEnv: (process.env.NODE_ENV ?? ''),
-  Port: (process.env.PORT ?? 0),
-  CookieProps: {
-    Key: 'ExpressGeneratorTs',
-    Secret: (process.env.COOKIE_SECRET ?? ''),
+  NODE_ENV: process.env.NODE_ENV ?? "",
+  PORT: process.env.PORT ?? 0,
+  COOKIES: {
+    Key: "ExpressGeneratorTs",
+    Secret: process.env.COOKIE_SECRET ?? "",
     // Casing to match express cookie options
     Options: {
       httpOnly: true,
       signed: true,
-      path: (process.env.COOKIE_PATH ?? ''),
+      path: process.env.COOKIE_PATH ?? "",
       maxAge: Number(process.env.COOKIE_EXP ?? 0),
-      domain: (process.env.COOKIE_DOMAIN ?? ''),
-      secure: (process.env.SECURE_COOKIE === 'true'),
+      domain: process.env.COOKIE_DOMAIN ?? "",
+      secure: process.env.SECURE_COOKIE === "true",
     },
   },
-  Jwt: {
-    Secret: (process.env.JWT_SECRET ??  ''),
-    Exp: (process.env.COOKIE_EXP ?? ''), // exp at the same time as the cookie
+  JWT: {
+    Secret: process.env.JWT_SECRET ?? "",
+    Exp: process.env.COOKIE_EXP ?? "", // exp at the same time as the cookie
   },
+  DB: {
+    port: process.env.DB_PORT ?? 27017,
+    user: process.env.DB_USER ?? "user",
+    pass: process.env.DB_PASS ?? "pass",
+    name: process.env.DB_NAME ?? "database",
+    host: process.env.DB_HOST ?? "localhost",
+  },
+  CORS_ORIGIN: process.env.CORS_ORIGIN ?? "http://localhost:3000",
 } as const;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,27 @@
-import "./pre-start"; // Must be the first import
-import logger from "jet-logger";
-
-import EnvVars from "@src/constants/EnvVars";
-import server from "./server";
+import EnvVars from "@src/constants/envVars";
+import app from "./app";
+import { disconnectDatabase } from "./util/database";
+import logger from "./util/logger";
 
 // **** Run **** //
 
+//These can be moved to a strings file if i18n is needed.
 const SERVER_START_MSG = "Express server started on port: " + EnvVars.Port.toString();
+const SERVER_CLOSE_MSG = "Express server shut down.";
 
-server.listen(EnvVars.Port, () => logger.info(SERVER_START_MSG));
+const server = app.listen(EnvVars.Port, () => logger.info(SERVER_START_MSG));
+
+const signals = ["SIGTERM", "SIGINT"];
+
+function gracefulShutdown(signal: string) {
+  process.on(signal, async () => {
+    server.close();
+    await disconnectDatabase();
+    logger.info(SERVER_CLOSE_MSG);
+    process.exit(0);
+  });
+}
+
+signals.forEach((signal) => {
+  gracefulShutdown(signal);
+});
