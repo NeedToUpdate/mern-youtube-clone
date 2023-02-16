c32d724eb3380b42abcf5af50ba0ae8a9dc4af05
Artem <updatethatpl0x@gmail.com>
2023-02-15 23:16:04 -0500

    added basic carousel

--- client/src/components/videos/VideoCard.tsx
+++ client/src/components/videos/VideoCard.tsx
@@ -19,8 +19,12 @@ export default function VideoCard(props: props) {
   //obviously you would want to implement some thumbnail logic. You would extract some frames from the video when its uploading
   //and save it as an image, then allow users to upload their own. Here we just give them a random one from a
   //random thumbnail generator
+
+  //another idea would be to store a gif of the video to have the slightly live play when you hover over the video
+
+  //also the autoplay feature of youtube could be implemented instead
   return (
-    <Link to={`/watch/${video.shortId}`} className="flex gap-2 w-[400px] flex-col rounded-md overflow-hidden justify-start m-2 shadow-sm hover:shadow-md hover:translate-y-[-1px] duration-200 ">
+    <Link to={`/watch/${video.shortId}`} className="flex gap-2 w-fit max-w-[400px] flex-col rounded-md overflow-hidden justify-start m-2 shadow-sm hover:shadow-md hover:translate-y-[-1px] duration-200 ">
       <img src={`/thumbnails/thumb${getThumbnailFromId(video.shortId)}.jpg`} alt={video.title} width={400} height={180} />
       <p className="font-bold text-lg capitalize ml-4">{video.title}</p>
     </Link>
--- client/src/components/videos/VideoCarousel.tsx
+++ client/src/components/videos/VideoCarousel.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-import { shuffle } from "../../utils/functions";
 import { Video } from "../../utils/Types";
 import VideoCard from "./VideoCard";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
+//this seems to be the best carousel maker
+//if this project had more time, it would be better to create
+//a carousel from scratch, as it could be much better for the project's needs
+import { Carousel } from "react-responsive-carousel";
+import { CSSProperties } from "react";
 interface props {
   sorting: "popular" | "hot" | "trending"; //this is just a template, not actually implemented, but it can be added to the backend search function
   videos: Video[];
@@ -11,12 +15,31 @@ interface props {
 
 export default function VideoCarousel(props: props) {
   const { videos, initialized, sorting } = props;
+  const indicatorStyles: CSSProperties = {
+    background: "#047857",
+    width: 8,
+    height: 8,
+    display: "inline-block",
+    margin: "-14px 8px",
+    borderRadius: "50%",
+    boxShadow: "0px 0px 1px 1px #d1fae5",
+  };
   return (
-    <div className="flex gap-1 flex-wrap">
+    <>
       {!initialized ? (
         <>loading...</>
       ) : (
-        <>
+        <Carousel
+          infiniteLoop
+          centerMode
+          centerSlidePercentage={30}
+          renderIndicator={(onClickHandler, isSelected, index, label) => {
+            if (isSelected) {
+              return <li style={{ ...indicatorStyles, background: "#00e1c3" }} aria-label={`Selected: ${label} ${index + 1}`} title={`Selected: ${label} ${index + 1}`} />;
+            }
+            return <li style={indicatorStyles} onClick={onClickHandler} onKeyDown={onClickHandler} value={index} key={index} role="button" tabIndex={0} title={`${label} ${index + 1}`} aria-label={`${label} ${index + 1}`} />;
+          }}
+        >
           {videos
             .sort((a, b) => {
               //this would usually handle some basic sorts or filters. here we are mocking some random sorts
@@ -31,8 +54,8 @@ export default function VideoCarousel(props: props) {
             .map((video) => {
               return <VideoCard key={video.shortId} video={video}></VideoCard>;
             })}
-        </>
+        </Carousel>
       )}
-    </div>
+    </>
   );
 }
